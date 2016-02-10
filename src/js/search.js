83795dc6cf7616f1c161efebd1e0324f25b954df
AnnatarHe <hele@iamhele.com>
2016-02-10 12:20:48 +0800

    search function is done

--- src/js/search.js
+++ src/js/search.js
@@ -1,59 +1,51 @@
-// Warning:
-// this script for search post by json that jekyll generated
-// this script is coding. DO NOT use it in production!!!
+// This script for search post by json that jekyll generated
 //
 // @Author  AnnatarHe
 // @Email   iamhele1994@gmail.com
 // @License MIT
-'use strict'
 
 // get the doms
-const searchBtn = document.querySelector('.search__action')
-const searchText = document.querySelector('.search__input')
-const resultDom = document.querySelector('.search__result--lists')
+(function() {
+    'use strict'
+    const searchBtn = document.querySelector('.search__action')
+    const searchText = document.querySelector('.search__input')
+    const resultDom = document.querySelector('.search__result--lists')
 
-// Search action. 
-// First of all. get the input by user.
-// Secondly. Using user input comparse with the json file
-// Finally. put it in dom!
-// Tips: innerHTML maybe good performance 
-const handleSearcher = () => {
-    new Promise((resolve, reject) => {
-        let searchVal = searchText.value
-        let result = []
-        for (let post of posts) {
-            if ( post.title.indexOf(searchVal) ) {
-                result.push(post)
+    // Search action. 
+    // First of all. get the input by user.
+    // Secondly. Using user input comparse with the json file
+    // Finally. put it in dom!
+    // Tips: innerHTML maybe good performance 
+    function handleSearcher() {
+        new Promise((resolve, reject) => {
+            let searchVal = searchText.value.toLowerCase().trim()
+            let res = posts.filter((item) => {
+                return (item.title.toLowerCase().indexOf(searchVal) > 0) || (item.url.toLowerCase().indexOf(searchVal) > 0)
+            })
+            resolve(res)
+        })
+        .then(res => {
+            // contact the dom string
+            let domStr = ''
+            for (let post of res ) {
+                domStr += `
+                <li>
+                    <a href="${post.url}">
+                        <span class="title">${post.title}</span>
+                        <span class="url">${post.url}</span>
+                    </a>
+                </li>
+                `
             }
-        }
-        resolve(result)
-    })
-    .then(res => {
-        // contact the dom string
-        let domStr = '<ul class="search-result">'
-        for (let post of res ) {
-            domStr += `
-            <li>
-                <a href="${post.url}">
-                    <span>${post.title}</span>
-                    <small>${post.url}</small>
-                </a>
-            </li>
-            `
-        }
-        domStr += '</ul>'
-        return domStr
-    })
-    .then(domStr => {
-        // render it!
-        resultDom.innerHTML = domStr
-    })
-}
-
-// listening double event.
-searchBtn.addEventListener('input', handleSearcher, false)
-searchBtn.addEventListener('keyup', (e) => {
-    if (e.keyCode == 13) {
-        handleSearcher()
+            return domStr
+        })
+        .then(domStr => {
+            // render it!
+            resultDom.innerHTML = domStr
+        })
     }
-}, false)
+
+    // listening double event.
+    searchText.addEventListener('input', handleSearcher, false)
+    searchBtn.addEventListener('click', handleSearcher, false)
+})()
--- src/js/search_trigger.js
+++ src/js/search_trigger.js
@@ -1,35 +1,43 @@
-'use strict'
+(function() {
+	'use strict'
+	const trigger = document.querySelector('.searchTrigger')
+	const close = document.querySelector('.search__close')
 
-const trigger = document.querySelector('.searchTrigger')
-const close = document.querySelector('.search__close')
+	const searchMaskBtn = document.querySelector('.search__mask')
+	const searchMask = searchMaskBtn.classList
 
-const searchMaskBtn = document.querySelector('.search__mask')
-const searchMask = searchMaskBtn.classList
+	trigger.addEventListener('click', () => {
+		searchMask.remove('hide')
+		searchMask.add('show')
+	},false)
 
-trigger.addEventListener('click', () => {
-	searchMask.remove('hide')
-	searchMask.add('show')
-},false)
+	// handle close event
+	searchMaskBtn.addEventListener('click', e => {
+		let targetClassList = e.target.classList
+		let isMaskClicked = targetClassList.contains('search__mask') ? true : false
+		let isCloseBtnClicked = targetClassList.contains('search__close') ? true : false
+		let isCloseIconClicked = targetClassList.contains('fa-close') ? true : false
+		// if clicked is target 
+		// then trigger close event
+		if (isMaskClicked || isCloseBtnClicked || isCloseIconClicked) {
+			handleCloseEvent()
+		}else {
+			return
+		}
+	}, false)
+	document.body.addEventListener('keydown', e => {
+		let eCode = e.keyCode
+		if (searchMask.contains('show') && eCode == 27) {
+			handleCloseEvent()
+		}else {
+			return
+		}
+	}, false)
 
-// handle close event
-searchMaskBtn.addEventListener('click', e => {
-	e.preventDefault()
-	let targetClassList = e.target.classList
-	let isMaskClicked = targetClassList.contains('search__mask') ? true : false
-	let isCloseBtnClicked = targetClassList.contains('search__close') ? true : false
-	let isCloseIconClicked = targetClassList.contains('fa-close') ? true : false
-	// if clicked is target 
-	// then trigger close event
-	if (isMaskClicked || isCloseBtnClicked || isCloseIconClicked) {
-		handleCloseEvent()
-	}else {
-		return
+	// handle close
+	// just css animation
+	function handleCloseEvent() {
+		searchMask.remove('show')
+		searchMask.add('hide')
 	}
-}, false)
-
-// handle close
-// just css animation
-function handleCloseEvent() {
-	searchMask.remove('show')
-	searchMask.add('hide')
-}
+})()
