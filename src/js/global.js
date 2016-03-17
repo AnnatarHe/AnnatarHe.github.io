823e055b1fd1470be117f4ca5dc31c575d1ebaae
AnnatarHe <j692356371@126.com>
2016-03-17 16:46:38 +0800

    migrate to https

---
+++ src/js/global.js
@@ -0,0 +1,7 @@
+(function() {
+    window.addEventListener('DOMContentLoaded', function() {
+        if ( window.location.protocol.indexOf('s') < 0) {
+            window.location.protocol = 'https';
+        }
+    });
+});
