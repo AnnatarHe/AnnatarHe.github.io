b028a5ccd8f39242a62ddf9d72902666ea1abbf0
AnnatarHe <hele@iamhele.com>
2016-04-14 15:56:43 +0800

    add visibilityState listener

--- src/js/global.js
+++ src/js/global.js
@@ -1,5 +1,17 @@
 (function() {
+    'use strict';
     if ( window.location.protocol.indexOf('s') < 0) {
         window.location.protocol = 'https'
     }
+
+    document.addEventListener("visibilitychange", (function() {
+        var title = document.title
+        return function() {
+            if (document.visibilityState == 'hidden') {
+                document.title = '别在其他网站瞎逛游了，快回来～'
+            }else {
+                document.title = title
+            }
+        }
+    })())
 })()
