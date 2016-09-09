17d59900e58ad95bf28504e467799ddf7d61afb8
AnnatarHe <iamhele1994@gmail.com>
2016-09-09 14:26:28 +0800

    add a post about ningjs and refactor global.js

--- src/js/global.js
+++ src/js/global.js
@@ -3,11 +3,7 @@
 export const titleShow = () => {
     var title = document.title
     return () => {
-        if (document.visibilityState == 'hidden') {
-            document.title = '别在其他网站瞎逛游了，快回来～'
-        }else {
-            document.title = title
-        }
+        document.title = document.visibilityState === 'hidden' ? '别在其他网站瞎逛游了，快回来～' : title
     }
 }
 
